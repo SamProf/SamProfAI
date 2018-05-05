b59d584a87aa1b662fe59dff9aa6f52d2554ccf7
Vladimir Samoilenko <wsamprof@gmail.com>
2018-05-05 12:47:54 +0200

    settings

--- src/app/components/world/core/world-model.ts
+++ src/app/components/world/core/world-model.ts
@@ -447,8 +447,8 @@ export class WorldSimSettings {
   botMemoryLength: number = 64;
   botCount: number = 64;
 
-  width: number = 30;
-  height: number = 20;
+  width: number = 64;
+  height: number = 48;
 
   eatingPercent: number = 0.2;
   poisonPercent: number = 0.1;
